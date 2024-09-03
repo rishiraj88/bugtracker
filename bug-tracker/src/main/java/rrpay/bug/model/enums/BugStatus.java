66fddaddfebc1f90101841a5395f5a05ec1c01b6
Rishi Raj <rishiraj88+github@gmail.com>
2024-09-03 08:11:09 +0200

    Propose a new state model for bug FSM.

--- bug-tracker/src/main/java/rrpay/bug/model/enums/BugStatus.java
+++ bug-tracker/src/main/java/rrpay/bug/model/enums/BugStatus.java
@@ -3,3 +3,14 @@ package rrpay.bug.model.enums;
 public enum BugStatus {
   OPEN, IN_PROGRESS,RESOLVED,NEED_INFO,CLOSED
 }
+
+/*
+ * TODO: to implement a finite state machine (FSM) corresponding to this state model:-
+ * defect / bug life cycle:
+ * New
+ * Assigned
+ * Active --> Rejected or Deferred
+ * Test --> Reopened --> Active
+ * Verified
+ * Closed
+ */
